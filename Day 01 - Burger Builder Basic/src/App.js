a0e49677291e052a92a8d5b0d4ebf2fdd8277068
ermohitjoshi <87078547+ermohitjoshi@users.noreply.github.com>
2022-07-08 22:09:06 +0530

    Add files via upload

---
+++ Day 01 - Burger Builder Basic/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Burger from './components/Burger';
+
+function App() {
+  return (
+    <div className="burgerContent">
+      <Burger/>
+    </div>
+  );
+}
+
+export default App;
